15c45fc418b9e87ef18e0bd9e0d779094f05c3b5
fode4cun <49346991+fode4cun@users.noreply.github.com>
2020-11-22 01:31:07 +0300

    getting fun with colors

--- builtins.py
+++ builtins.py
@@ -1,17 +1,22 @@
 import builtins
 from collections import defaultdict
 
+CBLUEBG2 = '\33[104m'
+CYELLOW2 = '\33[93m'
+CBOLD = '\33[1m'
+CEND = '\33[0m'
+
 types = defaultdict(list)
 
 for i in dir(builtins):
     types[type(getattr(builtins, i))].append(i)
 
 for key, val in types.items():
-    print(key, end=" :")
-    print('\n\tLength', len(val), end=": ")
+    print(f'{CBLUEBG2}{key}:{CEND}')
+    node_len = len(val)
+    print(f'\t{CYELLOW2}Length {node_len}{CEND}', end=":")
     for k, v in enumerate(val):
-        if k % 10 == 0:
-            print()
-            print('\t', end="")
-        print(v, end=", ")
+        if k % 5 == 0:
+            print('\n\t\t', end="")
+        print(f'{CBOLD}{v}{CEND}', end=", ")
     print()
