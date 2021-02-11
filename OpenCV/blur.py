67fcbe6dd176db28bd1819544ad387bfdd8de43d
Anton Dovnar <fode4cun@gmail.com>
2021-02-12 02:00:33 +0300

    Blur

--- OpenCV/blur.py
+++ OpenCV/blur.py
@@ -8,9 +8,10 @@ cv2.imshow(original, img)
 
 cv2.waitKey(10**3)
 
+modified = cv2.blur(img, ksize=(5, 5))
 blur = 'Blur image'
 cv2.namedWindow(blur)
 cv2.moveWindow(blur, 800, 30)
-blur_img = cv2.blur(img, ksize=(5, 5))
-cv2.imshow(blur, blur_img)
+cv2.imshow(blur, modified)
+
 cv2.waitKey(0)
