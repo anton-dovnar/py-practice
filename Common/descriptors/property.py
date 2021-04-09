3ddbddc47e4b5f1bff952ab15403826dfbb9254d
Anton Dovnar <fode4cun@gmail.com>
2021-04-09 22:45:39 +0300

    Property under hood

---
+++ Common/descriptors/property.py
@@ -0,0 +1,45 @@
+class Property:
+    "Emulate PyProperty_Type() in Objects/descrobject.c"
+
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError("unreadable attribute")
+        return self.fget(obj)
+
+    def __delete__(self, obj):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+
+    def getter(self, fget):
+        return type(self)(fget, self.fset, self.fdel, self.__doc__)
+
+    def setter(self, fset):
+        return type(self)(self.fget, fset, self.fdel, self.__doc__)
+
+    def deleter(self, fdel):
+        return type(self)(self.fget, self.fset, fdel, self.__doc__)
+
+
+class Cell:
+
+    def __init__(self, val):
+        self.val = val
+
+    @Property
+    def value(self):
+        return self.val
+
+
+if __name__ == "__main__":
+    cell = Cell(10)
+    print(cell.value)
