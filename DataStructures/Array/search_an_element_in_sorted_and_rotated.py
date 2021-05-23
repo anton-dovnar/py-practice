9bcd8215f1d59edf2df5af72b6d467dc36f40db4
Anton Dovnar <fode4cun@gmail.com>
2021-05-23 22:50:47 +0300

    Search an element in a sorted and rotated array

--- DataStructures/Array/search_an_element_in_sorted_and_rotated.py
+++ DataStructures/Array/search_an_element_in_sorted_and_rotated.py
@@ -4,27 +4,37 @@ Space complexity: O(1)
 """
 
 
-def search(arr, start, end, key):
-    if start > end:
-        return -1
+def search(arr, key):
+    left = 0
+    right = len(arr) - 1
 
-    mid = (start + end) // 2
-    if arr[mid] == key:
-        return mid
+    while left < right:
+        mid = (left + right) // 2
+        left_val = arr[left]
+        right_val = arr[right]
+        mid_val = arr[mid]
 
-    if arr[start] <= arr[mid]:
-        if arr[start] <= key <= arr[mid]:
-            return search(arr, start, mid - 1, key)
-        return search(arr, mid + 1, end, key)
+        if mid_val == key:
+            return mid
 
-    if arr[mid] <= key <= arr[end]:
-        return search(arr, mid + 1, end, key)
-    return search(arr, start, mid - 1, key)
+        if mid_val <= left_val:
+            if mid_val < key <= right_val:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        if mid_val >= left_val:
+            if left_val <= key < mid_val:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+    if arr[left] == key:
+        return left
+    return - 1
 
 
 if __name__ == '__main__':
-    arr = [4,5,6,7,0,1,2]
-    key = 3
-    start = 0
-    end = len(arr) - 1
-    print(search(arr, start, end, key))
+    arr = [4, 5, 6, 7, 0, 1, 2]
+    key = 7
+    print(search(arr, key))
